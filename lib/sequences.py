b1eab739e1f46968fe6e87264dd096dff8db0c68
jeff <jeff.etale@student.moringaschool.com>
2023-08-18 17:16:22 +0300

    done lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
+    sequence = []
+    a, b = 0, 1
+    for i in range(length):
+        sequence.append(a)
+        a, b = b, a + b
+    print(sequence)
     pass
